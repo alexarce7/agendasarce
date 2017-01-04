3a2dab2c573bb6271c15d2bbcb899d0797254cb7
alexarce7 <1714110116@utec-tgo.edu.mx>
2017-01-04 13:23:58 -0600

    agendas
    agenda

---
+++ agendas.sql
@@ -0,0 +1,20 @@
+create database agendas;
+
+use agendas;
+
+
+
+create table contactos(
+    
+id int not null AUTO_INCREMENT PRIMARY KEY, 
+    
+nombre varchar (50) not null,
+
+telefono varchar(10) not null,    
+email varchar(30) not null
+    
+);
+
+
+
+insert into contactos (nombre, telefono, email) values ('alejandro', '7751221098', 'alex@gmail.com');
---
+++ app.py
@@ -0,0 +1,90 @@
+import web
+# @
+from web import form
+db = web.database(dbn='mysql', db='agendas', user='root', pw='utec')
+render=web.template.render('templates')
+urls = (
+    
+    '/index','index',
+    '/nuevo', 'nuevo',
+    '/editar/(.+)','editar',
+    '/eliminar/(.+)','eliminar'
+)
+
+
+
+myformAgenda=form.Form(
+    form.Textbox('Nombre'), 
+    form.Textbox('Telefono'),
+    form.Textbox('Email'),
+    
+)
+class index:
+    def GET(self):
+        
+        result=db.select('contactos')
+        return render.index(result)
+    def POST(self):           
+        raise web.seeother("/nuevo")    
+class nuevo:
+    def GET(self):
+        formNew=myformAgenda()
+        return render.nuevo(formNew)
+    def POST(self): 
+        formNew = myformAgenda()
+        if not formNew.validates(): 
+            return render.nuevo(formNew)
+        else:
+            db.insert('contactos', nombre=formNew.d.Nombre, 
+            telefono=formNew.d.Telefono, email=formNew.d.Email)
+            result=db.select('contactos')
+            raise web.seeother('/index')
+            
+
+class editar:
+    def GET(self,id):
+        formEdit=myformAgenda()
+        
+        
+        result=db.select('contactos', where= "id=%s"%(id))
+        
+        for row in result:
+            formEdit['Nombre'].value=row.nombre
+            formEdit['Telefono'].value=row.telefono
+            formEdit['Email'].value=row.email
+            
+        return render.editar(formEdit)        
+    def POST(self,id):
+        formEdit=myformAgenda()
+        if not formEdit.validates(): 
+            return render.editar(formEdit)
+        else:
+            db.update('contactos', where='id=%s'%(id), nombre=formEdit.d.Nombre,
+             telefono=formEdit.d.Telefono, email=formEdit.d.Email)
+            result=db.select('contactos')
+            raise web.seeother('/index')
+class eliminar:
+    def GET(self,id):
+        formEdit=myformAgenda()
+        
+        result=db.select('contactos', where='id=%s'%(id))
+        
+        for row in result:
+            formEdit['Nombre'].value=row.nombre
+            formEdit['Telefono'].value=row.telefono
+            formEdit['Email'].value=row.email
+            
+        return render.eliminar(formEdit)        
+    def POST(self,id):
+        formEdit=myformAgenda()
+        if not formEdit.validates(): 
+            return render.eliminar(formEdit)
+        else:
+            db.delete('contactos', where="id=%s"%(id))
+            raise web.seeother('/index')
+
+
+if __name__ == "__main__":
+    
+    app = web.application(urls, globals())
+    app.run()
